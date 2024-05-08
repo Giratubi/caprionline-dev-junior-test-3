29ebdf15ade0586134ec7dd547fe9f3fb10191d9
mattia morini <vuotto@libero.it>
2024-05-08 17:40:36 +0200

    add new endpoints in controller
    #[Route('/genres', methods: ['GET'])]
    #[Route('/movies/genre/{genreId}', methods: ['GET'])]

--- backend/src/Controller/MoviesController.php
+++ backend/src/Controller/MoviesController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\MovieRepository;
+use App\Repository\GenreRepository;
+use App\Repository\MovieGenreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,15 +14,35 @@ class MoviesController extends AbstractController
 {
     public function __construct(
         private MovieRepository $movieRepository,
+        private  GenreRepository $genreRepository,
+        private MovieGenreRepository $movieGenreRepository,
         private SerializerInterface $serializer
     ) {}
 
     #[Route('/movies', methods: ['GET'])]
-    public function list(): JsonResponse
+    public function listMovies(): JsonResponse
     {
         $movies = $this->movieRepository->findAll();
         $data = $this->serializer->serialize($movies, "json", ["groups" => "default"]);
-
         return new JsonResponse($data, json: true);
+    }
+
+    #[Route('/genres', methods: ['GET'])]
+    public function listGenres(): JsonResponse
+    {
+        $genres = $this->genreRepository->findAll();
+        $data = $this->serializer->serialize($genres, "json");
+        return new JsonResponse($data, json:true);
+    }
+
+    #[Route('/movies/genre/{genreId}', methods: ['GET'])]
+    public function getMoviesByGenre(int $genreId): JsonResponse
+    {
+        $movies = $this->movieGenreRepository->findMoviesByGenre($genreId);
+        dump($movies);
+        if (empty($movies)) {
+            return $this->json(['message' => 'No movies found for this genre'], 404);
+        }
+        return $this->json($movies, context: ['groups' => ['default']]);
     }
 }
